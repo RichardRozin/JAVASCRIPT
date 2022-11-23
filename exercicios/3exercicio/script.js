7bfcc858d0f743f5b3493c2b14832e9c9e416018
RichardRozin <richardwkrozin@gmail.com>
2022-11-22 20:18:14 -0400

    exercicio 03

---
+++ exercicios/3exercicio/script.js
@@ -0,0 +1,23 @@
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0){
+        window.alert('ERRO! Por favor, digite um numero.')
+    }else{
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            c++
+            tab.appendChild(item)
+        }
+        
+    }
+    
+
+
+
+}
